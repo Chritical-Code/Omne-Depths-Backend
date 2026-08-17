ff760b480eb9de19a5883a220225a18c0c74c08e
chritical <christopherkbanks@gmail.com>
2026-08-17 18:35:13 -0400

    Learned how class based views and viewsets worked, thus removed my implementation of filtering for now.

--- content/views.py
+++ content/views.py
@@ -3,11 +3,11 @@ from .models import Topic, Post
 from .serializers import TopicSerializer, PostSerializer
 
 class TopicViewSet(viewsets.ModelViewSet):
-    queryset = Topic.objects.order_by('?')[:33]
+    queryset = Topic.objects.order_by("id")
     serializer_class = TopicSerializer
     permission_classes = [permissions.AllowAny]
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.order_by('?')[:10]
+    queryset = Post.objects.order_by("id")
     serializer_class = PostSerializer
     permission_classes = [permissions.AllowAny]
