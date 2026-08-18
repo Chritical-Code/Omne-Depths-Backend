d559e6f3ecc233111a1a8b2189b7ea29bee4fa91
chritical <christopherkbanks@gmail.com>
2026-08-18 15:35:51 -0400

    Added a view/url for showing posts of a specifc topic.

--- content/views.py
+++ content/views.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions, viewsets
 from .models import Topic, Post
 from .serializers import TopicSerializer, PostSerializer
+from rest_framework import generics
 
 class TopicViewSet(viewsets.ModelViewSet):
     queryset = Topic.objects.order_by("id")
@@ -11,3 +12,12 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.order_by("id")
     serializer_class = PostSerializer
     permission_classes = [permissions.AllowAny]
+
+class ListPostsOfTopic(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        topicID = self.kwargs.get("topicID")
+        data = Post.objects.filter(topic = topicID)
+        return data
--- omne_depths/urls.py
+++ omne_depths/urls.py
@@ -13,4 +13,5 @@ router.register("posts", content_views.PostViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("test/<int:topicID>/", content_views.ListPostsOfTopic.as_view())
 ]
